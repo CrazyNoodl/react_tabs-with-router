cb18232f1d2aa27ce8aab709e0274e9e409f2149
Rudenko Artem <artem.rud@bk.ru>
2019-11-11 20:43:17 +0200

    Updates

---
+++ precache-manifest.5addcb7ff7c0f153cefbdf103c5f3b24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "728919126b3afb3119c10421cf13203c",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "9a720203d0541d671250",
+    "url": "/react_tabs-with-router/static/css/main.142a4c88.chunk.css"
+  },
+  {
+    "revision": "378a83d46294be77d4c9",
+    "url": "/react_tabs-with-router/static/js/2.b10d0f43.chunk.js"
+  },
+  {
+    "revision": "9a720203d0541d671250",
+    "url": "/react_tabs-with-router/static/js/main.d3273222.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
